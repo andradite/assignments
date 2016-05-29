47c5f91d1223699698221e8f57213cfc183da170
ShadowfeindX <shadowfeind@programmer.net>
2016-05-29 04:10:39 +0000

    updated

--- lib/NAME.rb
+++ lib/NAME.rb
@@ -3,6 +3,4 @@
 class Main
     def self.run(*args)
     end
-    
-    run();
 end
--- tests/test_NAME.rb
+++ tests/test_NAME.rb
@@ -1,10 +1,19 @@
-require "./lib/NAME.rb"
+require "NAME.rb"
 require "test/unit"
 
 class TestNAME < Test::Unit::TestCase
 
-  def test_sample
-    assert_equal(4, 2+2)
-  end
+    def setup
+        $stdout = StringIO.new
+    end
+    
+    def teardown
+        $stdout = STDOUT
+    end
+    
+    def test_main
+        Main.run; output = $stdout.string.chomp
+        assert_equal('Hello, World!', output)
+    end
 
 end
